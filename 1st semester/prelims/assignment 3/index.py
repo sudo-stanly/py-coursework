ef09c6c57e68655b03c4969019d4dd7b85c3a2b1
stanly-sudo <alys.git24@gmail.com>
2025-08-18 21:18:25 +0800

    Data Types

---
+++ 1st semester/prelims/assignment 3/index.py
@@ -0,0 +1,10 @@
+"""
+    ! PYTHON DATA TYPES ASSIGNMENT:
+    
+    * converting variable type
+"""
+
+#* example 1:
+x = int(8700) #* -> int
+x = str(x) #* -> string
+print(x) #* OUTPUT: 8700 (don't forget the hashtag)
---
+++ 1st semester/prelims/lesson 2/index.py
@@ -0,0 +1,95 @@
+"""
+    ! Python Data Types
+"""
+
+#* TEXT TYPE:
+#x = str()
+
+#* NUMERIC TYPES:
+#x, y, z = int(), float(), complex()
+
+#* SEQUENCE TYPES:
+#x, y, z = list(), tuple(), range()
+
+#* MAPPING TYPE:
+#x = dict()
+
+#* SET TYPES:
+#x, y = set(), frozenset()
+
+#* BOOLEAN TYPE:
+#x = bool()
+
+#* BINARY TYPES:
+#x, y, z = bytes(), bytearray(), memoryview()
+
+
+
+"""
+    ! SETTING THE DATA TYPE
+"""
+
+# x = "I am a string" 
+# print(f"{type(x)} : {x}")
+
+# x = 1024
+# print(f"{type(x)} : {x}")
+
+# x = 20.5
+# print(f"{type(x)} : {x}")
+
+# x = 1j
+# print(f"{type(x)} : {x}")
+
+# x = ["apple", "banana", "cherry"]
+# print(f"{type(x)} : {x}")
+
+# x = ("apple", "banana", "cherry")
+# print(f"{type(x)} : {x}")
+
+# x = range(6)
+# print(f"{type(x)} : {x}")
+
+# x = {"name": "John Doe", "age" : 36}
+# print(f"{type(x)} : {x}")
+
+# x = {"apple", "banana", "cherry"}
+# print(f"{type(x)} : {x}")
+
+# x = frozenset({"apple", "banana", "cherry"})
+# print(f"{type(x)} : {x}")
+
+# x = True
+# print(f"{type(x)} : {x}")
+
+# x = b"Hello"
+# print(f"{type(x)} : {x}")
+
+# x = bytearray(5)
+# print(f"{type(x)} : {x}")
+
+# x = memoryview(bytes(5))
+# print(f"{type(x)} : {x}")
+
+# x = None
+# print(f"{type(x)} : {x}")
+
+
+"""
+    ! SETTING THE SPECIFIC DATA TYPE
+"""
+# x = str("Hello world") #* -> str
+# x = int(20) #* -> int
+# x = float(20.5) #* -> float
+# x = complex(1j) #* -> complex
+# x = list(("apple", "banana", "cherry")) #* -> list
+# x = tuple(("apple", "banana", "cherry")) #* -> tuple
+# x = range(6) #* -> range
+# x = dict(name="John", age=36) #* -> dict
+# x = set(("apple", "banana", "cherry")) #* -> set
+# x = frozenset(("apple", "banana", "cherry")) #* -> frozenset
+# x = bool(5) #* -> boolean
+# x = bytes(5) #* -> bytes
+# x = bytearray(5) #* -> bytearray
+# x = memoryview(bytes(5)) #* -> memoryview
+
